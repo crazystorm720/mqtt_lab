d21691e003ad072b55438414c45437ed588b53d7
Whisker Jones <medusa@aphrodite.atlantis.lan>
2024-06-12 14:12:03 -0600

    add new features

--- publisher.sh
+++ publisher.sh
@@ -3,12 +3,13 @@ BROKER_IP=${BROKER_IP:-mqtt-broker}
 TOPIC=${TOPIC:-sensors/data}
 
 while true; do
-  # Simulate real-world data (e.g., temperature and humidity)
-  TEMP=$((RANDOM % 30 + 10))
-  HUM=$((RANDOM % 50 + 30))
+  # Simulate real-world data (e.g., temperature and humidity with variability)
+  TEMP=$((RANDOM % 15 + 15))
+  HUM=$((RANDOM % 20 + 40))
   TIMESTAMP=$(date +%s)
   PAYLOAD="{\"timestamp\": \"$TIMESTAMP\", \"temperature\": $TEMP, \"humidity\": $HUM}"
-  
+
   mosquitto_pub -h $BROKER_IP -t $TOPIC -m "$PAYLOAD"
   sleep 1
 done
+
